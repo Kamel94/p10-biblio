fe0e41601e3cf9f34df04a9e75a56cf7db3bcf78
Kamel <a.kamel.square@gmail.com>
2020-08-12 13:15:29 +0200

    Ajout de la liste d'attente une fois qu'il n'y a plus d'exemplaire disponible, la liste d'attente ne peut comporter que 2x le nombre d'exemplaire et ajout de la mise à dispo pour la première personne sur la liste d'attente.

--- pret-service/src/main/java/fr/biblio/configuration/Constantes.java
+++ pret-service/src/main/java/fr/biblio/configuration/Constantes.java
@@ -4,5 +4,7 @@ public class Constantes {
 
     public static final String PRET = "PRET";
     public static final String RENDU = "RENDU";
+    public static final String EN_ATTENTE = "EN ATTENTE";
+    public static final String MIS_A_DISPO = "MIS A DISPO";
 
 }
